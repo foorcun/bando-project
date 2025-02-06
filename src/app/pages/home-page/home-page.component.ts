c970d7d9f778e12745d9b51c4f4c6e8d57e8b426
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-02-06 23:08:53 +0300

    confetti

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,16 +4,28 @@ import { FiyatlandirmaComponent } from './fiyatlandirma/fiyatlandirma.component'
 import { DisHijyenBeltComponent } from './dis-hijyen-belt/dis-hijyen-belt.component';
 import { PhoneButtonComponent } from './phone-button/phone-button.component';
 import { WhatsappButtonComponent } from './whatsapp-button/whatsapp-button.component';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  imports:[HeroComponentComponent, 
+  imports: [HeroComponentComponent,
     // VerilenHizmetlerComponent, 
     // FiyatlandirmaComponent, DisHijyenBeltComponent 
   ]
 })
 export class HomePageComponent {
+  ngOnInit(): void {
+    this.launchConfetti();
+  }
+
+  launchConfetti(): void {
+    confetti({
+      particleCount: 100,
+      spread: 160,
+      origin: { y: 0.6 }
+    });
+  }
 }
 
