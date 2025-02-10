95c2c81b9d64e78e97d1460e24f9e47e7c0b8a92
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-02-10 19:51:23 +0300

    ana sayfa slide

--- src/app/pages/home-page/hero-component/hero-component.component.ts
+++ src/app/pages/home-page/hero-component/hero-component.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero-component',
-  imports: [],
   templateUrl: './hero-component.component.html',
-  styleUrl: './hero-component.component.scss'
+  styleUrl: './hero-component.component.scss',
+  imports: [CommonModule]
 })
-export class HeroComponentComponent {
+export class HeroComponentComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  backgroundImages: string[] = [
+    '/images/hero-photo1.png',
+    '/images/hero-photo2.png',
+    '/images/hero-photo3.png'
+  ];
+  currentImageIndex: number = 0;
+  backgroundImage: string = this.backgroundImages[this.currentImageIndex];
+  nextBackgroundImage: string = this.backgroundImages[(this.currentImageIndex + 1) % this.backgroundImages.length];
+  isFading: boolean = false; // Controls fade animation
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Automatically change background every 5 seconds
+    setInterval(() => {
+      this.fadeToNextImage();
+    }, 5000);
+  }
 
   goContactPage() {
     this.router.navigate(['hizmetlerimiz-page']);
   }
 
+  fadeToNextImage() {
+    this.isFading = true;
+
+    setTimeout(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.backgroundImages.length;
+      this.backgroundImage = this.backgroundImages[this.currentImageIndex];
+      this.nextBackgroundImage = this.backgroundImages[(this.currentImageIndex + 1) % this.backgroundImages.length];
+      this.isFading = false;
+    }, 800); // Matches fade-out time
+  }
 }
+
+
