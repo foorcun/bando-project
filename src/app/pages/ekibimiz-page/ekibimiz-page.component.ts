e62cb1fbb4e7d8b981c5ef39a1763ee3ea49d645
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-02-20 19:35:54 +0300

    ekibimiz page

--- src/app/pages/ekibimiz-page/ekibimiz-page.component.ts
+++ src/app/pages/ekibimiz-page/ekibimiz-page.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ekibimiz-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ekibimiz-page.component.html',
   styleUrl: './ekibimiz-page.component.scss'
 })
 export class EkibimizPageComponent {
-
+  members = [
+    { name: 'Ali', role: 'Vokal', instagram: 'https://www.instagram.com/on7bando', twitter: '#' },
+    { name: 'Veli', role: 'Gitar', instagram: '#', twitter: '#' },
+    { name: 'Ayşe', role: 'Bas Gitar', instagram: '#', twitter: '#' },
+    { name: 'Mehmet', role: 'Bateri', instagram: '#', twitter: '#' },
+    { name: 'Zeynep', role: 'Klavye', instagram: '#', twitter: '#' }
+  ];
 }
